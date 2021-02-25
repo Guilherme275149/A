a70650fb19eabbc31ee9dda0edefe76785264bfc
Guilherme275149 <78052265+Guilherme275149@users.noreply.github.com>
2021-02-25 08:10:53 -0300

    Add files via upload

---
+++ src/cara.js
@@ -0,0 +1,46 @@
+const cara = (pushname, prefix, botName, ownerName) => { 
+	return `🔰 -----[ *Guia De Usuario ${botName}* ]----- 🔰
+Roiii, ${pushname} 👋
+Aqui está como usá-lo *${botName}*
+         ────────────────
+Se você não entende, leia primeiro -_-
+         ────────────────
+➸ *${prefix}sticker*
+Envie fotos / vídeos com adesivos de legenda
+➸ *${prefix}ttp*
+Texto Para Adesivo Ex: ${prefix}ttp Lann ID
+➸ *${prefix}tts*
+Exemplo de voz do Google: ${prefix}tts id Hallo Ramlan
+➸ *${prefix}toimg*
+Responda ao adesivo que você deseja transformar na imagem
+➸ *${prefix}nulis*
+Escrever um livro
+➸ *${prefix}stalkig*
+Stalking instagram contoh : ${prefix}stalkig iamramlan_
+➸ *${prefix}quotes*
+Random quotes
+➸ *${prefix}bikinquote*
+Membuat quotes contoh : ${prefix}bikinquote Kamu gay & Ramlan
+➸ *${prefix}play*
+Baixar músicas do YouTube pode usar texto ou links
+➸ *${prefix}yutubdl*
+Mendownload video dari youtube, gunakan link ya kak
+➸ *${prefix}tiktod*
+Mendownload video tiktok, gunakan link kak
+➸ *${prefix}hartatahta*
+Membuat lomgo, contoh : ${prefix}hartatahta Nadia
+➸ *${prefix}pornhub*
+Membuat logo, contoh : ${prefix}pornhub Ramlan & Hub
+Note : untuk logo maker / image maker jika undefined berarti harus memakai &
+➸ *${prefix}fitnah*
+Only grup untuk fitnah orang, contoh : ${prefix}fitnah @tagtarget & Hai & Hai juga
+➸ *${prefix}mutual*
+Gacha nomor yang ada di database / yang menggunakan bot
+         ────────────────
+Note : Sisanya gunain otak kalian aja cape w jelasin :)
+         ────────────────
+🔰 -----[ *TUMTOR BY RAMLAN ID* ]----- 🔰
+`
+}
+
+exports.cara = cara
